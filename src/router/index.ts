a8ecabc59be7e1fa76b865fbcef33753b4c9310e
demarcj <demarcjohnson@gmail.com>
2022-10-23 03:29:59 -0400

    Created an error page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,14 +2,17 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import HomeView from 'src/views/HomeView.vue';
 import FontSizeView from 'src/views/FontSizeView.vue';
 import FontWeightView from 'src/views/FontWeightView.vue';
+import ErrorView from 'src/views/ErrorView.vue';
+
+const environment = window.location.hostname.includes(`localhost`) ? `` : `css-style-maker`;
 
 const routes: Array<RouteRecordRaw> = [
-    { path: '/', component: HomeView },
-    { path: '/font-size', component: FontSizeView },
-    { path: '/font-weight', component: FontWeightView },
-    { path: '/:notFound(.*)', redirect: '/'}
+    { path: `${environment}/`, component: HomeView },
+    { path: `${environment}/font-size`, component: FontSizeView },
+    { path: `${environment}/font-weight`, component: FontWeightView },
+    { path: `/:notFound(.*)`, component: ErrorView }
 ]
-
+  
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
---
+++ src/views/ErrorView.vue
@@ -0,0 +1,28 @@
+<template>
+  <view-content title="404 Page Not Found">
+    <div>Check the top menu on the left for the 'Reference' tab and picking a page.</div> 
+    <div>Or see the full size version of the app by clicking here 
+      <router-link
+        class="home-link"
+        :to="environment"
+      >Home</router-link>.
+    </div>
+  </view-content>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data() {
+    return{
+      environment: window.location.hostname.includes(`localhost`) ? `/` : `/css-style-maker`
+    }
+  }
+})
+</script>
+<style scoped>
+  a.home-link{
+    text-decoration: underline;
+    font-weight: 700;
+  }
+</style>
