0ac1601d7d773ab8af78f109616490f17efa9ba8
demarcj <demarcjohnson@gmail.com>
2022-10-23 03:35:38 -0400

    This should fix the error of the environment variable

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,10 +4,10 @@ import FontSizeView from 'src/views/FontSizeView.vue';
 import FontWeightView from 'src/views/FontWeightView.vue';
 import ErrorView from 'src/views/ErrorView.vue';
 
-const environment = window.location.hostname.includes(`localhost`) ? `` : `css-style-maker`;
+const environment = window.location.hostname.includes(`localhost`) ? `/` : `/css-style-maker`;
 
 const routes: Array<RouteRecordRaw> = [
-    { path: `${environment}/`, component: HomeView },
+    { path: `${environment}`, component: HomeView },
     { path: `${environment}/font-size`, component: FontSizeView },
     { path: `${environment}/font-weight`, component: FontWeightView },
     { path: `/:notFound(.*)`, component: ErrorView }
